c3cadfe61ec808d328a2747683c2fe3f6968f6ab
hsh28084 <56817606+hsh28084@users.noreply.github.com>
2022-01-18 20:21:29 +0900

    project init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import MainPageComponent from './main';
+import UploadPage from './upload';
+import ProductPage from './product';
+import {Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/"> 
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/upload"> 
+          <UploadPage />
+        </Route>
+        <Route exact={true} path="/products/:id"> 
+          <ProductPage />
+        </Route>
+      </Switch>
+    </div>
+    
+  );
+		
+}
+
+
+
+export default App;
+
+
---
+++ src/child.js
@@ -0,0 +1,5 @@
+function ChildComponent(){
+	return <h3>나는 자식입니다.</h3>
+}
+
+export default ChildComponent;
